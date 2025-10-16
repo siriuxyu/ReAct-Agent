d2ef07f6d61098f63943cbf10d74460a6885f09b
Siriux Yu <hengjia.y@outlook.com>
2025-10-16 16:14:36 -0700

    [chore] fixed string bug

--- QA-with-translate/QA_with_Trans.py
+++ QA-with-translate/QA_with_Trans.py
@@ -17,14 +17,14 @@ class State(TypedDict):
 llm = ChatAnthropic(model="claude-3-5-haiku-20241022")
 
 def llm_1(state: State) -> State:
-    prompt = f"Please answer the following question with your: {state["input_text"]}.  Requirements: you should be honest if you don't know the answer, and please don't guess the solution."
+    prompt = f"Please answer the following question with your: {state['input_text']}.  Requirements: you should be honest if you don't know the answer, and please don't guess the solution."
     llm_response = llm.invoke(
         [{"role": "user", "content": prompt}]
     )
     return {"step1_output": llm_response.content}
 
 def llm_2(state: State) -> State:
-    prompt = f"Please translate the following text to {state["language"]}: {state["step1_output"]}.  Requirements: you should follow the original text and don't add additional information."
+    prompt = f"Please translate the following text to {state['language']}: {state['step1_output']}.  Requirements: you should follow the original text and don't add additional information."
     llm_response = llm.invoke(
         [{"role": "user", "content": prompt}]
     )
