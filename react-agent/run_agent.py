67441e02300ff585e5e4a06268c21f810b7a549a
Siriux Yu <hengjia.y@outlook.com>
2025-10-22 01:12:44 -0700

    [chore] Fixes.

--- react-agent/run_agent.py
+++ react-agent/run_agent.py
@@ -1,7 +1,6 @@
 import asyncio
 from graph import graph
 from context import Context
-from langchain_core.messages import AIMessageChunk
 
 async def main_token_stream():
 
@@ -9,16 +8,7 @@ async def main_token_stream():
         {"messages": [{"role": "user", "content": "what is the weather in San Diego?"}]},
         context=Context(system_prompt="You are a helpful AI assistant.")
     ):
-        if "call_model" in chunk:
-            message_chunk = chunk["call_model"]["messages"][-1]
-            print(message_chunk)
-            if isinstance(message_chunk, AIMessageChunk):
-                print(message_chunk.content, end="", flush=True)
-            elif message_chunk.tool_calls:
-                print(f"\n--- Calling Tools: {message_chunk.tool_calls} ---")
-
-    print("\n--- End of Stream ---")
-
+        print(chunk)
 
 if __name__ == "__main__":
     asyncio.run(main_token_stream())
